f9e2895bd35797716ad36bb54854b1f5c5da7e02
Jing Ma <ma.jing@outlook.com>
2017-05-31 13:00:19 +0200

    Gittest

---
+++ src/main/java/com/wixsite/jingmacv/model/GITTEST.java
@@ -0,0 +1,7 @@
+package com.wixsite.jingmacv.model;
+
+public class GITTEST {
+	public static void main(String[] args) {
+		System.out.println("GITTEST!");
+	}
+}
--- src/main/java/com/wixsite/jingmacv/model/Test.java
+++ src/main/java/com/wixsite/jingmacv/model/Test.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test {
 	
@@ -13,26 +15,66 @@ public class Test {
 		System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.nationalevacaturebank.nl/");
+		driver.get("https://www.monsterboard.nl");
 
-		// If there's a cookie message, it'll be clicked.
-		if (driver.findElements(By.cssSelector("#form_save")).size() != 0)
-			driver.findElement(By.cssSelector("#form_save")).click();
-		
 		// Finds and fills in input field "what".
-		WebElement input = driver.findElement(By.cssSelector(".form-group input.ng-empty"));		
+		WebElement input = driver.findElement(By.cssSelector("#q1"));		
 		input.sendKeys("java");
 		// Finds and fills in input field "where".
-		input = driver.findElement(By.cssSelector(".search-location-group input.ng-empty"));
+		input = driver.findElement(By.cssSelector("#where1"));
 		input.sendKeys("den haag");
 		// Finds and submits the web form.
-		WebElement submit = driver.findElement(By.cssSelector(".search-button button"));
+		WebElement submit = driver.findElement(By.cssSelector("#doQuickSearch"));
 		submit.click();
 		
-		// If there's a "next" button, it'll be clicked.
-		List<WebElement> list = driver.findElements(By.cssSelector("li .arrow"));
-		if (list.size() != 0) {
-			list.get(1).click();
+		// Waits for the cookie message to appear and then clicks it.
+		WebDriverWait wait = new WebDriverWait(driver, 3000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.fa-times"))).click();
+
+		int pageCount = 0;
+		while (true) {
+			System.out.println(++pageCount);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".js_result_container.primary")));
+			List<WebElement> list = driver.findElements(By.cssSelector(".js_result_container.primary"));
+			System.out.println("Size: " + list.size());
+			// Loops over the vacancies.
+			for (WebElement item : list) {
+				WebElement jobTitleTag = null;
+				WebElement companyTag = null;
+				WebElement locationTag = null;
+				String jobTitle = null;
+				String company = null;
+				String location = null;
+				// Finds the job title, company and location per vacancy.
+				if (item.findElements(By.cssSelector(".jobTitle")).size() != 0) {
+					jobTitleTag = item.findElement(By.cssSelector(".jobTitle"));
+					jobTitle = jobTitleTag.getText();
+				}
+				if (item.findElements(By.cssSelector(".company")).size() != 0) {
+					companyTag = item.findElement(By.cssSelector(".company"));
+					company = companyTag.getText();
+				}
+				if (item.findElements(By.cssSelector(".job-specs-location")).size() != 0) {
+					locationTag = item.findElement(By.cssSelector(".job-specs-location"));
+					location = locationTag.getText();
+				}
+				// If the company is unknown the user would still want to apply to the job.
+				if (jobTitle != null && location != null)
+					System.out.println(jobTitle + ", " + company + ", " + location + ", " + pageCount);
+				
+			}
+			// Checks for layover and clicks it if it exists.
+			WebElement layover = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".popup-close-icon")));
+			if (layover.isDisplayed()) {
+				layover.click();
+			}
+			// Waits for the "next" button to appear and then clicks it.
+			if (!wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".next"))).isDisplayed()) {
+				System.out.println("BREAK!");
+				break;
+			}
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".next"))).click();
 		}
+		driver.close();
 	}
 }
