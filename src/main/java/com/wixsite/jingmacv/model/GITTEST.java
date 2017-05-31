abc902b692fad8e1c913749ddd1eb4b1c484ed8f
Jing Ma <ma.jing@outlook.com>
2017-05-31 13:02:12 +0200

    Update Gittest.java

--- src/main/java/com/wixsite/jingmacv/model/GITTEST.java
+++ src/main/java/com/wixsite/jingmacv/model/GITTEST.java
@@ -2,6 +2,6 @@ package com.wixsite.jingmacv.model;
 
 public class GITTEST {
 	public static void main(String[] args) {
-		System.out.println("GITTEST!");
+		System.out.println("GITTEST! HAHA");
 	}
 }
